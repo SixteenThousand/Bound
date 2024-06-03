543725772c177eccca3937781f8b92e77c7dd9d5
Thomas Lindop <thomas16256@gmail.com>
2024-06-03 23:47:23 +0100

    >> bugfix: actually utilise DurationToString in the timer display

--- src/core.go
+++ src/core.go
@@ -9,7 +9,6 @@ const MINS_PER_HR = 60
 const SECS_PER_MIN = 60
 
 func Run() {
-    println("We got here!")
     durr, err := time.ParseDuration("2m")
     if err != nil {
         fmt.Println("Oh no, we got here.")
@@ -22,26 +21,23 @@ func DurationToString(d time.Duration) string {
     var hrs, mins, secs int
     hrs = Floor(d.Hours())
     mins = Floor(d.Minutes()) - (MINS_PER_HR * hrs)
-    secs = (Floor(d.Seconds()) - 
-        (SECS_PER_MIN * MINS_PER_HR * hrs) -
-        (SECS_PER_MIN * mins))
+    secs = Floor(d.Seconds()) % SECS_PER_MIN
     return fmt.Sprintf("%02dh %02d' %02d\"",hrs,mins,secs)
 }
 
 func RunTimer(d time.Duration) {
     end := time.Now().Add(d)
     endMsg := make(chan int,1)
-    time.AfterFunc(d,func() {
-        endMsg <- 1
-    })
+    time.AfterFunc(d,func() { endMsg <- 1 })
     go func() {
         for {
             if len(endMsg) > 0 {
                 break
             }
-            fmt.Printf("\rTime remaining: %s",time.Until(end).String())
+            fmt.Printf("\rTime remaining: %s",
+                DurationToString(time.Until(end)))
             time.Sleep(100 * time.Millisecond)
         }
     }()
-    println(<- endMsg)
+    <- endMsg
 }
